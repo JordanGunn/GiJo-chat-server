7eb59ead3f9af5292211e9a248cff0d7b74732ba
Data Comm <dc@milliways.bcit.ca>
2022-04-06 15:45:22 -0700

    create folder, file for voice_arecord

---
+++ voice_arecord/voice_arecord.c
@@ -0,0 +1,6 @@
+//
+// Created by root on 2022-04-06.
+//
+
+#include "voice_arecord.h"
+
---
+++ voice_arecord/voice_arecord.h
@@ -0,0 +1,11 @@
+//
+// Created by root on 2022-04-06.
+//
+
+#ifndef CPT_VOICE_ARECORD_H
+#define CPT_VOICE_ARECORD_H
+
+void record(int fd);
+void play(int fd);
+
+#endif //CPT_VOICE_ARECORD_H
