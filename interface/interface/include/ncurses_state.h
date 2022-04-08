6dfb72533429cb8724789672b60fe683106624d8
Web-CW <wei.charlie@hotmail.com>
2022-04-08 13:57:57 -0700

    merge conflicts fixed

---
+++ interface/interface/include/ncurses_state.h
@@ -0,0 +1,8 @@
+//
+// Created by charlie on 2022-04-08.
+//
+
+#ifndef CPT_NCURSES_STATE_H
+#define CPT_NCURSES_STATE_H
+
+#endif //CPT_NCURSES_STATE_H
---
+++ interface/interface/src/ncurses_state.c
@@ -0,0 +1,5 @@
+//
+// Created by charlie on 2022-04-08.
+//
+
+#include "ncurses_state.h"
